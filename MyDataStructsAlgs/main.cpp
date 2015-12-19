6d32e7110010d158855bb0669c678d47936953ed
Sebastian Hernandez <cbasshernandez@gmail.com>
2015-12-19 18:19:31 -0500

    Initial Commit

---
+++ MyDataStructsAlgs/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MyDataStructsAlgs
+//
+//  Created by Sebastian Hernandez on 12/19/15.
+//  Copyright © 2015 Sebastian Hernandez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
